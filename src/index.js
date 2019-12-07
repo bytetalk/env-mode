d54bdfed131c022a9979db9cb87dc42bd9917c59
bytetalk <bytetalk@qq.com>
2019-12-07 22:08:07 +0800

    .env parse 'true' & 'false' & number

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ function log (message) {
     console.log(`[set-node-env][debug] ${message}`)
   }
 }
+function getValue (value) {
+  // Assigning a property on process.env will implicitly convert the value to a string.
+  // This behavior is deprecated.
+  // Future versions of Node.js may throw an error when the value is not a string, number, or boolean.
+  if (value === 'true') {
+    return true
+  } else if (value === 'false') {
+    return false
+  } else {
+    const number = Number(value)
+    if (isNaN(number)) {
+      return value
+    } else {
+      return number
+    }
+  }
+}
 function parseEnv (name, content = '') {
   const ret = {}
   content.split(END_OF_LINE).forEach((line, index) => {
@@ -17,8 +34,11 @@ function parseEnv (name, content = '') {
     ] = line.split(KEY_VALUE_SEPARATOR, 2)
     key = key.trim()
     value = value.trim()
+    if (key.startsWith('#')) {
+      return
+    }
     if (key && value) {
-      ret[key] = value
+      ret[key] = getValue(value)
     } else if (key + value) {
       log(`parsing ${name} line:${index + 1}, value:'${line}' is illegal format`)
     }
